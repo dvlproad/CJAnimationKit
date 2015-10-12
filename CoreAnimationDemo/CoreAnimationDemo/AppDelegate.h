e93fc072f9e6ecfcb194e70e4d9ecca0433fc682
dvlproad <lichaoqian@MacBook-Pro.local>
2015-10-13 01:43:34 +0800

    basic和keyframe

---
+++ CoreAnimationDemo/CoreAnimationDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CoreAnimationDemo
+//
+//  Created by 李超前 on 15/10/12.
+//  Copyright © 2015年 李超前. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CoreAnimationDemo/CoreAnimationDemo/Controller/BasicAnimationVC.h
@@ -0,0 +1,13 @@
+//
+//  BasicAnimationVC.h
+//  CoreAnimationDemo
+//
+//  Created by 李超前 on 15/10/12.
+//  Copyright © 2015年 李超前. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BasicAnimationVC : UIViewController
+
+@end
